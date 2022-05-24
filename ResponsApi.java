52d621b1a0f726d3834c480c98e3967482cbd45c
Lina Aloqaily <ogililina@gmail.com>
2022-05-24 08:33:42 +0300

    exercises

---
+++ ResponsApi.java
@@ -0,0 +1,17 @@
+package model.springex;
+
+public class ResponsApi {
+    private String msg;
+
+    public ResponsApi(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
